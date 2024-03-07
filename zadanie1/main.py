696369f143bf68352211f5f0df0f6e49b7df544c
Bartosz Podemski <bartekpodemski@gmail.com>
2024-03-07 13:51:30 +0100

    charts

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -12,4 +12,21 @@ stop = menu.stopChoice()
 
 calculations = Calculations(function[1])
 bisekcja = Bisekcja(calculations, edges[0], edges[1], stop[1])
-print(bisekcja.calculate(stop[0], function[0]))
+result = bisekcja.calculate(stop[0], function[0])
+print(result)
+
+x_values = np.linspace(edges[0], edges[1], 100)
+if function[0] == 1:
+    y_values = calculations.horner(x_values)
+elif function[0] == 2:
+    y_values = calculations.trigonometry(x_values)
+elif function[0] == 3:
+    y_values = calculations.exponential(x_values)
+
+plt.plot(x_values, y_values, label="f(x)")
+plt.scatter(result[0], 0, color="red", label="Punkt zerowy")
+plt.axhline(0, color="black", lw=0.5)
+plt.axvline(result[0], color="green", lw=0.5)
+plt.grid(color="gray", linestyle="--", linewidth=0.5)
+plt.legend()
+plt.show()
